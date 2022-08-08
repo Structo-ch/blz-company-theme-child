ebd60a220e950758f97375222dc67483bff152d7
Tungdil213 <admin@herric.de>
2022-08-09 00:08:53 +0200

    add categories attributes color

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Theme functions and definitions
  *
@@ -10,7 +11,8 @@
  *
  * @return void
  */
-function hello_elementor_child_enqueue_scripts() {
+function hello_elementor_child_enqueue_scripts()
+{
 	wp_enqueue_style(
 		'hello-elementor-child-style',
 		get_stylesheet_directory_uri() . '/dist/app.css',
@@ -20,6 +22,45 @@ function hello_elementor_child_enqueue_scripts() {
 		'1.0.0'
 	);
 
-   wp_enqueue_script( 'my-app', get_stylesheet_directory_uri() . '/dist/app.js', array(), '1.0.0', true );
+	wp_enqueue_script('my-app', get_stylesheet_directory_uri() . '/dist/app.js', array(), '1.0.0', true);
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20);
+add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20);
+
+
+
+add_action('elementor/query/request_category_spiritueux', function ($query) {
+	$query = new WP_Query(array(
+		'post_type' => 'product',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'product_cat',
+				'field'    => 'slug',
+				'terms'    => 'spiritueux',
+				'include_children' => true,
+			),
+		),
+	));
+	var_dump($query);
+});
+
+add_action('elementor/query/request_category_bieres', function ($query) {
+	$query = new WP_Query(array(
+		'post_type' => 'product',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'product_cat',
+				'field'    => 'slug',
+				'terms'    => 'bieres',
+				'include_children' => true,
+			),
+		),
+	));
+	var_dump($query);
+});
+
+remove_action('woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10);
+remove_action('woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10);
+add_action('woocommerce_before_subcategory', function ($category) {
+	echo '<a href="' . esc_url(get_term_link($category, 'product_cat')) . '" data-color="' . get_field('couleur', $category)  . '">';
+}, 20);
