da7210001bb138fd16866aabf91b08b222932e98
Tungdil213 <admin@herric.de>
2022-08-15 01:01:42 +0200

    wip slider

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,15 @@
-console.log('Start Hello-Elementor')
+import Swiper from 'swiper';
+
+console.log('Start Hello-Elementor');
+//now let's see if we can call it
+(function ($) {
+  $(document).ready(() => {
+    console.log('slider');
+    const swiper = new Swiper(
+      '#rn-tiny-slider .elementor-shortcode > .elementor',
+      { wrapperClass:'elementor',loop: true }
+    );
+    console.log('Riko', swiper);
+    
+  });
+})(jQuery);
--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ function hello_elementor_child_enqueue_scripts()
 		'1.0.0'
 	);
 
-	wp_enqueue_script('my-app', get_stylesheet_directory_uri() . '/dist/app.js', array(), '1.0.0', true);
+	wp_enqueue_script('my-app', get_stylesheet_directory_uri() . '/dist/app.js', array( 'jquery', 'elementor-frontend' ), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20);
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20);
@@ -62,5 +62,16 @@ add_action('elementor/query/request_category_bieres', function ($query) {
 remove_action('woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10);
 remove_action('woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10);
 add_action('woocommerce_before_subcategory', function ($category) {
-	echo '<a href="' . esc_url(get_term_link($category, 'product_cat')) . '" data-color="' . get_field('couleur', $category)  . '">';
+	echo '<a href="' . esc_url(get_term_link($category, 'product_cat')) . '" style="background-color:' . get_field('couleur', $category)  . ';">';
+}, 20);
+
+remove_action( 'woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10 );
+add_action('woocommerce_before_subcategory', function ($category ) {
+	?>
+	<h2 class="woocommerce-loop-category__title" style="color:<?php echo get_field('text-color', $category); ?>;">
+		<?php
+		echo esc_html( $category->name );
+		?>
+	</h2>
+	<?php
 }, 20);
