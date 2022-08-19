0f81f2acc1787014fdb74314efc58f044ad92b55
Tungdil213 <admin@herric.de>
2022-08-19 20:40:50 +0200

    rework scss folder, add postcss and autoprefixer

--- build.js
+++ build.js
@@ -1,12 +1,22 @@
 import esbuild from 'esbuild'
-import sassPlugin from 'esbuild-plugin-sass'
+import {sassPlugin} from 'esbuild-sass-plugin'
+import postcss from 'postcss' ;
+import autoprefixer from 'autoprefixer' ;
 
 console.log("build scripts files")
 esbuild.build({
     entryPoints: ['assets/scripts/app.js'],
     bundle: true,
     minify: true,
-    plugins: [sassPlugin()],
+    plugins: [
+        sassPlugin({
+          async transform(source, resolveDir) {
+            const {css} = await postcss([autoprefixer]).process(source, {from: undefined})
+            return css
+          }
+        }),
+        //...
+      ],
     outfile: 'dist/app.js'
 })
 .catch((e) => console.error(e.message))
--- functions.php
+++ functions.php
@@ -22,12 +22,10 @@ function hello_elementor_child_enqueue_scripts()
 		'1.0.0'
 	);
 
-	wp_enqueue_script('my-app', get_stylesheet_directory_uri() . '/dist/app.js', array( 'jquery', 'elementor-frontend' ), '1.0.0', true);
+	wp_enqueue_script('my-app', get_stylesheet_directory_uri() . '/dist/app.js', array('jquery', 'elementor-frontend'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20);
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20);
-
-
 
 add_action('elementor/query/request_category_spiritueux', function ($query) {
 	$query = new WP_Query(array(
@@ -65,13 +63,15 @@ add_action('woocommerce_before_subcategory', function ($category) {
 	echo '<a href="' . esc_url(get_term_link($category, 'product_cat')) . '" style="background-color:' . get_field('couleur', $category)  . ';">';
 }, 20);
 
-remove_action( 'woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10 );
-add_action('woocommerce_before_subcategory', function ($category ) {
-	?>
+remove_action('woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10);
+add_action('woocommerce_before_subcategory', function ($category) {
+?>
 	<h2 class="woocommerce-loop-category__title" style="color:<?php echo get_field('text-color', $category); ?>;">
 		<?php
-		echo esc_html( $category->name );
+		echo esc_html($category->name);
 		?>
 	</h2>
-	<?php
+<?php
 }, 20);
+
+//fonction pour afficher le titre d'un custom post
