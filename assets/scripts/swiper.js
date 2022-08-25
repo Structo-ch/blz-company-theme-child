1e5fe79f8623b56e4ccafeb6957e97b8e7c5edb9
Tungdil213 <admin@herric.de>
2022-08-26 00:22:03 +0200

    carousel on product home

--- assets/scripts/swiper.js
+++ assets/scripts/swiper.js
@@ -1,47 +1,71 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-console.log('Start Swiper');
-
-const swiperContainer = '.mySwiper .elementor-shortcode';
-
-const NodeSwiperContainer = document.querySelectorAll(swiperContainer);
-if (NodeSwiperContainer.length > 0) {
-  const buttonsClass = {
-    swiperNext: 'swiper-button-next',
-    swiperPrev: 'swiper-button-prev',
-    swiperPagination: 'swiper-pagination',
-  };
-
-  Object.values(buttonsClass).forEach((buttonClass) => {
-    const nodeButton = document.createElement('div');
-    nodeButton.classList.add(buttonClass);
-    NodeSwiperContainer[0].appendChild(nodeButton);
-  });
-
-  new Swiper(swiperContainer, {
-    loop: true,
-    speed: 300,
-    slide: 1,
-    slidesPerView: 1,
-    centeredSlides: true,
+const swiperHeaderHome = {
+  swiperContainer: '.mySwiper .elementor-shortcode',
+  settings: {
     wrapperClass: 'elementor-260',
-    grabCursor: true,
+  }
+};
+const swiperProductHome = {
+  swiperContainer: '.disable-elementor-grid .woocommerce',
+  settings: {
+    slidesPerView: 3,
+    wrapperClass: 'products',
+  }
+};
 
-    pagination: {
-      el: `.${buttonsClass.swiperPagination}`,
-      clickable: true,
-    },
+document.querySelectorAll('.disable-elementor-grid .woocommerce li.product').forEach(
+  product => {
+    product.classList.add('swiper-slide')
+  }
+)
 
-    navigation: {
-      nextEl: `.${buttonsClass.swiperNext}`,
-      prevEl: `.${buttonsClass.swiperPrev}`,
-    },
 
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true,
-    },
-  });
-}
+
+const swipers = [swiperProductHome, swiperHeaderHome];
+
+swipers.forEach((swiper) => {
+  const swiperContainer = swiper.swiperContainer;
+
+  const NodeSwiperContainer = document.querySelectorAll(swiperContainer);  
+  if (NodeSwiperContainer.length > 0) {
+    const buttonsClass = {
+      swiperNext: 'swiper-button-next',
+      swiperPrev: 'swiper-button-prev',
+      swiperPagination: 'swiper-pagination',
+    };
+
+    Object.values(buttonsClass).forEach((buttonClass) => {
+      const nodeButton = document.createElement('div');
+      nodeButton.classList.add(buttonClass);
+      NodeSwiperContainer[0].appendChild(nodeButton);
+    });
+
+    const defaultSettings = {
+      loop: true,
+      speed: 300,
+      slidesPerView: 1,
+      centeredSlides: true,
+      grabCursor: true,
+    
+      pagination: {
+        el: `.${buttonsClass.swiperPagination}`,
+        clickable: true,
+      },
+    
+      navigation: {
+        nextEl: `.${buttonsClass.swiperNext}`,
+        prevEl: `.${buttonsClass.swiperPrev}`,
+      },
+    
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+    }
+    
+    new Swiper(swiperContainer, {...defaultSettings, ...swiper.settings } );
+  }
+});
--- functions.php
+++ functions.php
@@ -10,3 +10,17 @@ require_once(dirname(__FILE__) ."/includes/scripts-js-css.php");
 require_once(dirname(__FILE__) ."/includes/elementor-query.php");
 require_once(dirname(__FILE__) ."/includes/categories.php");
 require_once(dirname(__FILE__) ."/includes/single-product.php");
+
+
+function filter_woocommerce_post_class($classes, $product)
+{
+    // is_product() - Returns true on a single product page
+    // NOT single product page, so return
+    if (!is_product()) return $classes;
+
+    // Add new class
+    $classes[] = 'swiper-slide';
+
+    return $classes;
+}
+add_filter('woocommerce_post_class', 'filter_woocommerce_post_class', 10, 2);
